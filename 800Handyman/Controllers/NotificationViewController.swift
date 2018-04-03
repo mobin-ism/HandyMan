b22571a62a34990ba05750db4073e9170e6c9707
piash <piash1465@gmail.com>
2018-04-03 18:05:26 +0600

    basic structure created

---
+++ 800Handyman/Controllers/NotificationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationViewController.swift
+//  800Handyman
+//
+//  Created by Tanvir Hasan Piash on 3/4/18.
+//  Copyright © 2018 Tanvir Hasan Piash. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        navigationController?.navigationBar.setGradientBackground(colors: [NAV_GRADIENT_TOP, NAV_GRADIENT_BOTTOM])
+    }
+    
+}
